4bcfdad5d84c0dbd489ae6967623d226066e93e5
spycrab <wialex121@gmail.com>
2025-03-09 22:04:32 -0400

    Style: apply whitespace formatting

--- marvel-app/src/components/populateData.js
+++ marvel-app/src/components/populateData.js
@@ -1,38 +1,44 @@
 export function displayCharacters(characters){
+    const contentWrapper = document.querySelector('.display_content'); 
 
     const fragment = document.createDocumentFragment();
-    const charactersContainer = document.querySelector('.display_characters'); 
+
+    const characterWrapper = document.createElement('div'); 
+    characterWrapper.classList.add('display_characters'); 
 
     characters.forEach((character) => {
-        
-        const characterWrapper = document.createElement('div');
-
-        const characterImage = document.createElement('div'); 
-        const characterInformation = document.createElement('div'); 
-        const characterName = document.createElement('span'); 
-
-        characterWrapper.classList.add('character');
-        
-        characterImage.classList.add('character_image'); 
-        characterInformation.classList.add('character_information');  
-        characterName.classList.add('information_name'); 
-
-        characterName.textContent = character.name; 
-
+        // generate img and populate with thumbnail
         const imgElement = new Image();
         const thumbnail = `${character.thumbnail.path}.${character.thumbnail.extension}`; 
         imgElement.src = thumbnail; 
         imgElement.alt = character.name;
+        
+        const characterElement = document.createElement('div');
+        characterElement.classList.add('character');
+
+        const characterImage = document.createElement('div'); 
+        characterImage.classList.add('character_image'); 
+
+
+        const characterInformation = document.createElement('div'); 
+        characterInformation.classList.add('character_information');  
+
+        const characterInformationName = document.createElement('span'); 
+        characterInformationName.classList.add('information_name'); 
+
+        characterInformationName.textContent = character.name; 
+
 
         characterImage.appendChild(imgElement); 
-        characterInformation.appendChild(characterName);        
-        // character image and name
-        characterWrapper.appendChild(characterImage); 
-        characterWrapper.appendChild(characterInformation);  
+        characterInformation.appendChild(characterInformationName);    
 
-        fragment.appendChild(characterWrapper); 
+        characterElement.appendChild(characterImage); 
+        characterElement.appendChild(characterInformation);    
+
+        fragment.appendChild(characterElement); 
     });
 
-    charactersContainer.appendChild(fragment); 
+    characterWrapper.appendChild(fragment);
+    contentWrapper.appendChild(characterWrapper);  
 
 }
