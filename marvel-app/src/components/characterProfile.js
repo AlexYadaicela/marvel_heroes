9cca8898333451ade2294382516d8bfe893015c7
spycrab <wialex121@gmail.com>
2025-03-11 15:51:07 -0400

    fix: character page not retriving data

--- marvel-app/src/components/characterProfile.js
+++ marvel-app/src/components/characterProfile.js
@@ -1,30 +1,27 @@
 import '../styles/heroProfiles.css'; 
 
-import { getTs } from "../main";
-import { getHash } from "../main"; 
-import { getPublicKey } from "../main";
-import { getBaseUrl } from "../main";
-
-export async function generateCharacterPage(character){
-    console.log(character.id); 
-    try{
-    const url = `${getBaseUrl()}/characters/${character.id}/comics?ts=${getTs()}&apikey=${getPublicKey()}&hash=${getHash()}&limit=5`; 
-    const response = await fetch (url); 
-    if(!response.ok){
-        throw new Error(`Response status: ${response.status}`); 
-    }
-
-    const res = await response.json(); 
-    console.log('comics');
-    console.log(res); 
-    characterPage(character, res.data.results); 
-
-}catch(error){
-    console.error(error.message); 
-    }
+export async function generateCharacterPage(baseUrl, ts, publickey, hash, character){
+    console.log('in generate page', character); 
+    // try{
+    // const url = `${baseUrl}/characters/${characterDetails.id}/comics?ts=${ts}&apikey=${publickey}&hash=${hash}&limit=6`; 
+    // const response = await fetch (url); 
+    // if(!response.ok){
+    //     throw new Error(`Response status: ${response.status}`); 
+    // }
+    // const res = await response.json(); 
+    // console.log('comics', res); 
+    // characterPage(character, res.data.results); 
+    // }catch(error){
+    //     console.error(error.message); 
+    // }
 }
 
-function characterPage(character, comics){
+export function characterPage(character){
+    console.log('in character page', character); 
+    
+}
+export function characterPage(character){
+    console.log(character);
     // clear current dom and populate element with current character selection
     const displayContent = document.querySelector('.display_content');
     displayContent.innerHTML = ''; 
@@ -33,20 +30,29 @@ function characterPage(character, comics){
     const profile = document.createElement('div'); 
     profile.classList.add('character_profile'); 
 
-    const characterInformation = document.createElement('div'); 
+    const characterInformation = document.createElement('div');
+
+    characterInformation.classList.add('character_details'); 
 
     const characterImage = document.createElement('div'); 
     const characterName = document.createElement('div'); 
     const spanName = document.createElement('span'); 
+    const spanDetail = document.createElement('span'); 
 
     spanName.textContent = character.name; 
-    
-    const img = new Image(); 
-    img.src = character.thumbnail; 
-    img.alt = character.name; 
 
-    characterImage.appendChild(img); 
+    if(character.description === '' || character.description === ' '){
+        spanDetail.textContent = 'Description not available'; 
+    }else{
+        spanDetail.textContent = character.description;
+    }
+
+    spanDetail.classList.add('character_description'); 
+
+    characterImage.style.backgroundImage = `url('${character.thumbnail}')`;
+    
     characterName.appendChild(spanName); 
+    characterName.appendChild(spanDetail); 
 
     characterInformation.appendChild(characterImage); 
     characterInformation.appendChild(characterName); 
@@ -59,21 +65,22 @@ function characterPage(character, comics){
     const comicFragment = document.createDocumentFragment(); 
     
     // iterate through comics and set up a div for each comic
-    comics.forEach((comic) => {
-        const comicElement = document.createElement('div'); 
-        
-        const img = new Image(); 
-        img.src = `${comic.thumbnail.path}.${comic.thumbnail.extension}`;
-        
-        comicElement.appendChild(img); 
-        
-        comicFragment.appendChild(comicElement); 
-    });
-    comicsWrapper.appendChild(comicFragment);
+    if(Number(character.numOfComics) !== 0){
+        comics.forEach((comic) => {
+            const comicElement = document.createElement('div'); 
+            
+            const img = new Image(); 
+            img.src = `${comic.thumbnail.path}.${comic.thumbnail.extension}`;
+            
+            comicElement.appendChild(img); 
+            
+            comicFragment.appendChild(comicElement); 
+        });
+        comicsWrapper.appendChild(comicFragment);
+    }
     
     profile.appendChild(characterInformation); 
     profile.appendChild(comicsWrapper); 
-
     fragment.appendChild(profile); 
 
     displayContent.appendChild(fragment);
